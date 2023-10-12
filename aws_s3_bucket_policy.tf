0675ae693cb4bd888cf3808a1618fb0657b374f9
Walid Karray <walid.karray@gmail.com>
2023-10-12 20:37:02 +0200

    wip

--- aws_s3_bucket_policy.tf
+++ aws_s3_bucket_policy.tf
@@ -3,23 +3,9 @@ resource "aws_s3_bucket_policy" "allow_cloudfront" {
   policy = data.aws_iam_policy_document.cloudfront.json
 }
 
-
 data "aws_iam_policy_document" "cloudfront" {
-  #  statement {
-  #    actions = [
-  #      "s3:ListBucket",
-  #    ]
-  #    resources = [
-  #      aws_s3_bucket.static_website.arn,
-  #    ]
-  #    principals {
-  #      type = "AWS"
-  #      identifiers = [
-  #        aws_cloudfront_origin_access_identity.current.iam_arn,
-  #      ]
-  #    }
-  #  }
 
+  // Allows read-only access to a CloudFront Origin Access Identity (OAI) on the S3
   statement {
     actions = [
       "s3:GetObject",
@@ -36,24 +22,25 @@ data "aws_iam_policy_document" "cloudfront" {
     }
   }
 
-  #  statement {
-  #    sid    = "AllowSSLRequestsOnly"
-  #    effect = "Deny"
-  #    actions = [
-  #      "s3:*",
-  #    ]
-  #    condition {
-  #      test     = "Bool"
-  #      variable = "aws:SecureTransport"
-  #      values   = ["false"]
-  #    }
-  #    principals {
-  #      type        = "*"
-  #      identifiers = ["*"]
-  #    }
-  #    resources = [
-  #      aws_s3_bucket.static_website.arn,
-  #      "${aws_s3_bucket.static_website.arn}/*"
-  #    ]
-  #  }
+  // denies any and all non-SSL requests to the S3 bucket, ensuring encrypted transit for all requests.
+  statement {
+    sid    = "AllowSSLRequestsOnly"
+    effect = "Deny"
+    actions = [
+      "s3:*",
+    ]
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    resources = [
+      aws_s3_bucket.static_website.arn,
+      "${aws_s3_bucket.static_website.arn}/*"
+    ]
+  }
 }
